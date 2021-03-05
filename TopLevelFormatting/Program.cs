45ad9f4e825add0f75599444d9b42ec3cb131e01
Phil Scott <phil.scott@gmail.com>
2021-03-05 15:36:43 -0500

    bug repo

---
+++ TopLevelFormatting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+var s = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+
+Console.WriteLine("Hello World!");
+
+Console.WriteLine($"Today's date is {s}");
+
+{
+    var otherS = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+
+    Console.WriteLine("Hello World, again!");
+
+    Console.WriteLine($"Today's date is probably still {otherS}");
+}
